f75f97f98a287cf1ff690d3e514aa6fa14381a41
Dmitry Suzdalev <dimsuz@gmail.com>
2013-06-09 01:56:08 +0400

    Add customizations

---
+++ .emacs-custom.el
@@ -0,0 +1,17 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(css-indent-offset 2)
+ '(custom-safe-themes (quote ("501caa208affa1145ccbb4b74b6cd66c3091e41c5bb66c677feda9def5eab19c" "d2622a2a2966905a5237b54f35996ca6fda2f79a9253d44793cfe31079e3c92b" default)))
+ '(ido-enable-flex-matching t)
+ '(ido-enable-tramp-completion nil)
+ '(js-indent-level 2)
+ '(scss-compile-at-save nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(flymake-errline ((t (:background "misty rose" :foreground "indian red" :underline nil :weight normal)))))
